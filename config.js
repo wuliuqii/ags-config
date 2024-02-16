d30311572b03fc204613be225d7ceaa1420e14d9
gallon <h1090703848@gmail.com>
2024-02-16 13:32:40 +0800

    init my ags config

---
+++ config.js
@@ -0,0 +1,1 @@
+export default (await import('./modules/main.js')).default;
---
+++ modules/misc/clock.js
@@ -0,0 +1,19 @@
+import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import { clock } from '../variables.js';
+
+/**
+ * @param {import('types/widgets/label').Props & {
+ *   format?: string,
+ *   interval?: number,
+ * }} o
+ */
+export default ({
+    format = '%H:%M:%S %B %e. %A',
+    ...rest
+} = {}) => Widget.Label({
+    class_name: 'clock',
+    label: clock.bind('value').transform(time => {
+        return time.format(format) || 'wrong format';
+    }),
+    ...rest,
+});
